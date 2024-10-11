11aea05629fcc68fa669ebd55021271ab08b49eb
Arifin-only <arifin180104@gmail.com>
2024-10-11 15:43:31 +0800

    Create Closure.dart

---
+++ Closure.dart
@@ -0,0 +1,21 @@
+void main() {
+
+    var counter = 0;
+    
+    void increment(){
+        print('Increment');
+        counter++;
+    }
+    
+    
+    print(counter);
+    
+    
+    increment();
+    
+    increment();
+    
+    
+    print(counter);
+
+}
